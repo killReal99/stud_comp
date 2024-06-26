d851f12b050b3abd915606c13d7065f5150f9925
Kirill Latyshov <kirilllatyshov99@gmail.com>
2024-06-26 15:16:36 +0300

    add define

--- src/main.cpp
+++ src/main.cpp
@@ -1,9 +1,11 @@
 #include<iostream>
 #include "add.h"
 
+#define Y 5
+
 using namespace std;
 
 int main() {
-  int x = 10, y = 5;
-  cout << "Multiplication is " << sum(x, y) << endl;
+  int x = 10;
+  cout << "Addition is " << sum(x, Y) << endl;
 }
