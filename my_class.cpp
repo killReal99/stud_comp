54e2696fb014530b52e330366bfc03c7bd973198
Kirill Latyshov <kirilllatyshov99@gmail.com>
2024-06-26 13:20:40 +0300

    init

---
+++ my_class.cpp
@@ -0,0 +1,10 @@
+#include "my_class.h" // заголовочный файл, расположенный в локальной директории
+#include <iostream> // заголовочный файл стандартной библиотеки
+
+using namespace N;
+using namespace std;
+
+void my_class::do_something()
+{
+    cout << "Doing something!" << endl;
+}
