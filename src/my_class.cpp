4942ef4b06d4cef040cd6c65dafa6868b28524dc
Kirill Latyshov <kirilllatyshov99@gmail.com>
2024-06-26 13:48:19 +0300

    Add macros

--- src/my_class.cpp
+++ src/my_class.cpp
@@ -1,10 +1,12 @@
 #include "my_class.h" // заголовочный файл, расположенный в локальной директории
 #include <iostream> // заголовочный файл стандартной библиотеки
 
+#define AMOUNT 5 // макро параметр
+
 using namespace N;
 using namespace std;
 
 void my_class::do_something()
 {
-    cout << "Doing something!" << endl;
+    cout << "Doing something! " << AMOUNT << " times a day" endl;
 }
